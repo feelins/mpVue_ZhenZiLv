1516deff798718bb9b191f2f61793f7aab869d38
feelins <feipengshao@163.com>
2022-04-17 10:50:46 +0800

    在意见反馈里，上传图片的功能实现

--- src/pages/opinion/opinion.vue
+++ src/pages/opinion/opinion.vue
@@ -11,6 +11,24 @@
                <label class="word-count">{{word_count}}/200</label>
              </div>
           </div>
+          <div class="row">
+            <div>
+              <label class="name">相关截图（选填）</label>
+              <label class="img-count">{{img_count}}/2</label>
+            </div>
+            <label v-for="(item, index) in src" :key="index">
+              <img :src="item" class="img">
+            </label>
+            <label v-if="img_count<2" @click="uploadPicture">
+              <img class="img" src="../../../static/images/addimage.png">
+            </label>
+          </div>
+          <div class="row">
+            <div class="name">
+              微信号（选填）
+            </div>
+            <input v-model="wechat" maxlength="20" placeholder="如果想详细交流，请留下微信号哦" class="wechat-input">
+          </div>
         </div>
         <button>提交</button>
     </div>
@@ -21,12 +39,37 @@
     data () {
       return {
         opinion: '',
-        word_count: 0
+        word_count: 0,
+        src: [],
+        img_count: 0,
+        wechat: ''
       }
     },
     watch: {
       opinion () {
         this.word_count = this.opinion.length
+      },
+      src () {
+        this.img_count = this.src.length
+      }
+    },
+    methods: {
+      uploadPicture () {
+        let that = this
+        wx.chooseImage({
+          count: 2,
+          sizeType: ['original', 'compressed'],
+          sourceType: ['album', 'camera'],
+          success (res) {
+            // tempFilePath可以作为img标签的src属性显示图片
+            const tempFilePaths = res.tempFilePaths
+            // 在这里直接使用this.src，其实是调用的wx.chooseImage这个对象
+            // this.src
+            // 正确的方法应该是先将this，在上文保存一下。
+            that.src.push(tempFilePaths)
+            console.log('path: ', that.src)
+          }
+        })
       }
     }
   }
